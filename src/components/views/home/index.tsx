a58d3f7bc255de3ce67d8ffdbc387e362b1e074f
Rosa V. Cordero Ozoria <rcordero@SERDOM.COM>
2022-04-06 10:11:14 -0400

    FEACT: change select option for select with images inside

--- src/components/views/home/index.tsx
+++ src/components/views/home/index.tsx
@@ -1,35 +1,101 @@
-import { useContext } from 'react';
+
+import './home.css';
+import { useContext, useState } from 'react';
 import { ListOfNews } from '../../common/ListOfNews'
 import { NewsContext } from '../../../context/newsProvider';
 import { INewObj } from '../../../interfaces/interfaces';
 import getNews from '../../../services/getNews';
 import useLocalStorage from '../../../hooks/useLocalStorage';
 
-import './home.css'
+import arrow from '../../../assets/img/Arrow.png';
+import Reactjs from '../../../assets/img/React.png';
+import Vuejs from '../../../assets/img/Vuejs.png';
+import Angular from'../../../assets/img/Angular.png'
 
-const news_name = ['Angular', 'Reacts', 'Vuejs'];
+const news_name = ['Angular', 'Reactjs', 'Vuejs'];
 
 export const Home: React.FunctionComponent = () => {
 
   const {news, setNewNews} = useContext(NewsContext)
-  const [item, saveItem] = useLocalStorage('lastSearch');  
+  //const [item, saveItem] = useLocalStorage('lastSearch');  
+  const [search, setSearch] = useLocalStorage('latestSearch', 'Select your news');
+  const [show, setShow] = useState(false)
 
   //handle change event from option search news
-  const handleChange=(event: React.ChangeEvent<HTMLSelectElement>)=>{
-    let name = event.target.value;
-    getNews({ keyword: name }).then((newsRes) => {
+  // const handleChange=(event: React.ChangeEvent<HTMLSelectElement>)=>{
+  //   let name = event.target.value;
+  //   getNews({ keyword: name }).then((newsRes) => {
+  //     const newsArray= newsRes[0];
+  //     setNewNews(newsArray as INewObj[]);
+  //   });
+  //   saveItem('lastSearch', event.target.value)
+  // }
+
+  const showOptions =()=>{
+    setShow(bol => !bol);
+  }
+
+  const handleClick=(event: React.SyntheticEvent)=>{
+    const {target} =event;
+    console.log((target as HTMLButtonElement).dataset.value);
+    const name = (target as HTMLButtonElement).dataset.value;
+    if(!!name)
+    {
+      setShow(bol => !bol);
+      getNews({ keyword: name }).then((newsRes) => {
       const newsArray= newsRes[0];
       setNewNews(newsArray as INewObj[]);
     });
-    saveItem('lastSearch', event.target.value)
+    setSearch('latestSearch', name);
+  }
   }
 
   return (
     <>
       <div>
+        
       <form>
         <div className='searchBy'>
-          <select onChange={handleChange} defaultValue={item}>
+        <div className='selector'>
+          <div className='selector__field' onClick={showOptions}>
+            <p>{search}</p>
+            <img 
+              src={arrow} 
+              alt='arrow icon to open select'
+              className={`${show && 'arrow--rotate'}`}
+            />
+          </div>
+          <div>
+          <ul className={`options__list ${!show && 'hide'}`}>
+            {
+              news_name.map((name, i)=>{
+                return (
+                  <li
+                  key={name+i}
+                  value={name}
+                  className='options'
+                  onClick={handleClick}
+                  data-value={name}
+                  >
+                    <img src={
+                      name === 'Angular' ? Angular
+                      : name === 'Reactjs' ? Reactjs
+                      : Vuejs
+                    } 
+                    alt={`${name} icon`}
+                    data-value={name}
+                    />
+                    <p
+                     data-value={name}>
+                       {name}
+                    </p>
+                  </li>
+              )})
+            }
+          </ul>
+          </div>
+        </div>
+          {/* <select onChange={handleChange} defaultValue={item}>
             <option value='' disabled>Select your news</option>
             {
               news_name.map((name, i)=>(<option
@@ -38,7 +104,7 @@ export const Home: React.FunctionComponent = () => {
                 name
               }</option>))
             }
-          </select>
+          </select> */}
         </div>
       </form>
       </div>
